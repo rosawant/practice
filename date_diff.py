77dfbf3fc5fba3b3dc77ea2ebde1b99605b43ef3
roshansawant70@yahoo.com <roshansawant70@yahoo.com>
2017-07-26 00:40:56 +0530

    added next_day procedure

--- date_diff.py
+++ date_diff.py
@@ -1,3 +1,13 @@
 #starting program to calculate
 #difference between two dates
-
+ 
+def next_day(year,month,day):
+	
+ 	if day < 30:
+ 		return year,month,day +1
+ 	else:
+ 		if month < 12:
+ 			return year,month + 1,1
+ 		else:
+ 			return year + 1,1,1
+print next_day(2014,5,2)
